621a64b6b4225d748f09289eaf15095b4f8ece33
JakobSchweighoferGit <jakobschweighofer2609@gmail.com>
2025-10-13 16:15:44 +0800

    Initial commit

---
+++ BankDataAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankDataAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
